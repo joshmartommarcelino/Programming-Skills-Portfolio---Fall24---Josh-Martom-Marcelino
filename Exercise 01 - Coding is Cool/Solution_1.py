77063e268b32e5bd51cf36c19aef78ff632a3882
Josh Martom Marcelino <johnsh2508@gmail.com>
2024-11-02 13:59:02 +0400

    Day 1 progress

---
+++ Exercise 01 - Coding is Cool/Solution_1.py
@@ -0,0 +1,20 @@
+## Exercise 1: Coding is Cool- 10 Marks
+
+#Fill in the blanks in the Python code below to output the phrase **"Coding is Cool"** to the console using variables and string concatenation.
+
+
+### Fill in the blanks below
+#```python
+#word1 = 
+#word2 = 
+#word3 =
+# Use string concatenation to combine the variables and print the phrase
+#_____(_________)
+
+###Solution_1
+
+word_1=("Coding ")
+word_2=("is ")
+word_3=("Cool")
+
+print(word_1 + word_2 + word_3)
---
+++ Exercise 02- Simple Sums/Solution_2.py
@@ -0,0 +1,18 @@
+## Exercise 2: Simple Sums - 15 Marks
+
+##In this exercise, you will create and work with integer variables, perform arithmetic operations, and print the result to the console.
+
+### Steps:
+#1. Declare a variable and initialize it with the integer value `8`.
+#2. Declare a second variable and initialize it with the integer value `10`.
+#3. Declare a third variable that stores the sum of first two numbers.
+#4. Print the value of the sum to the console.
+
+###Solution_2
+
+A = int(8)
+B = int(10)
+
+sum = A + B
+
+print(f"The answer is: " ,(sum))
