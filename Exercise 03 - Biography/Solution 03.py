67688806806e9759627bf58f248459d49b157ad3
Josh Martom Marcelino <johnsh2508@gmail.com>
2024-11-04 09:34:53 +0400

    Day 2.1
    using dictionaries and coming the print statements into 1 line using escape characters I simplified the code and also used try and except if the user inputs the word of the number instead of the nummerical characters

---
+++ Exercise 03 - Biography/Solution 03.py
@@ -0,0 +1,35 @@
+## Exercise 3: Biography - 25 Marks
+
+#In this exercise, you'll create a program that stores and prints your name, hometown, and age to the console using a Python dictionary.
+
+### Steps:
+#1. Store the information (name, hometown, and age) as key-value pairs in a dictionary.
+#2. Print the values on separate lines using a single `print()` statement.
+#3. Use variables with appropriate data types for each piece of information.
+
+
+
+### Advanced Requirements:
+#Have the user input their name, hometown, and age instead of hardcoding the values.
+#Try giving both your first and second name when asked for your name. What happens? How can you handle multiple words in Python?
+#Test the program by entering a string value for age (e.g., "twenty"). What happens? How can you prevent this issue?
+
+name = (input("Enter your name: "))
+
+hometown = (input("Enter your hometown: "))
+
+try: 
+  age = int(input("Enter your age: "))
+except ValueError:
+  print("You must enter a number for your age")
+
+
+Biography = {
+  'Name': name,
+  "Hometown": hometown,
+  "Age": age,
+}
+print(f"Your Name is: {Biography['Name']}\nYour Hometown is: {Biography['Hometown']}\nYour Age is: {Biography['Age']}")
+
+
+
