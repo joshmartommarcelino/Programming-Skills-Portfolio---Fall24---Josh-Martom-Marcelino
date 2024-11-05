cc6a363fba727b974c5d9ce9dd69f6f1fe5b427e
Josh Martom Marcelino <johnsh2508@gmail.com>
2024-11-05 14:43:05 +0400

    day 3.9 Solution_7
    using 5 for loops and changing the values and increments I successfully made the countdowns work.

--- Exercise 07 - Some Counting/Solution_7.py
+++ Exercise 07 - Some Counting/Solution_7.py
@@ -8,3 +8,19 @@
 #* Write a loop that counts down from 50 to 10 in decrements of 2.
 #* Write a loop that counts up from 100 to 200 in increments of 5.
 #*You may include all loops in a single project*
+
+print("0 to 50")
+for i in range(0,51,1):
+    print(i)
+print("50 to 0")
+for i in range(50,-1,-1):
+    print(i)
+print("30 to 50")
+for i in range(30,51,1):
+    print(i)
+print("50 to 10")
+for i in range(50,8,-2):
+    print(i)
+print("100 to 200")
+for i in range(100,201,5):
+  print(i)
