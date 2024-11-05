80e2b76318917376c1959e21afd637c8c3f5762e
Josh Martom Marcelino <johnsh2508@gmail.com>
2024-11-05 14:23:43 +0400

    day 3.7
    I have finished the password break by using while loops and nested if else statments.

--- Exercise 06 -Brute Force Attack/Solution_6.py
+++ Exercise 06 -Brute Force Attack/Solution_6.py
@@ -12,3 +12,20 @@
 
 #Modify the program to include a maximum of 5 password attempts. If the user enters the wrong password, inform them of the remaining attempts. 
 #If the maximum number of attempts is reached, inform the user that the authorities have been alerted.
+
+real_password = "12345"
+
+attempts = 0
+max_attempts = 5
+
+while attempts < max_attempts:
+    password = input("Enter the password: ")
+
+    if password == real_password:
+          print("Access has been granted, Welcome User!")
+    else:
+        attempts += 1
+        if attempts == 5:
+            print("You have attempted to enter 5 times but have failed, You are now locked out of this device")
+        else:
+            print(f"Incorrect password. { 5 - attempts} attempts remaining.")
