e631bc52b094a557fa3df50a317b85a987b58f45
Josh Martom Marcelino <johnsh2508@gmail.com>
2024-11-06 09:13:04 +0400

    day 4.2 Solution 9
    its very easy all I had to do was print hello in the def command so it can call it just bust typing the word hello.

---
+++ Exercise 09 -Hello/Solution_9.py
@@ -0,0 +1,13 @@
+## Exercise 9: Hello - 10 Marks
+
+#Fill in the blanks in the code below so that the function hello() prints "Hello" to the console.
+#```python
+
+def hello():
+    print("hello")  # Fill in this blank to print "Hello" to the console
+
+def main():
+    hello # Fill in this blank to call the hello() function
+
+if __name__ == "__main__":
+    main()
